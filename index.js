422069c8c5496e6002e031857198765f09b4374e
Srihimamshu <109665007+Srihimamshu@users.noreply.github.com>
2023-11-21 23:54:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,81 +1,61 @@
-let userForm = document.getElementById("user-form");
-
-
-const retrievedEntries = () => {
-    let entries = localStorage.getItem("user-entries");
-    if (entries) {
-        entries = JSON.parse(entries);
-    } else {
-        entries = [];
-    }
-    return entries;
-}
-
-let userEntries = retrievedEntries();
-
-const displayEntries = () => {
-    const entries = retrievedEntries();
-    const tableEntries = entries.map((entry) => {
-        const nameCell = `<td class='border px-4 py-2'>${entry.name}</td>`;
-        const emailCell = `<td class='border px-4 py-2'>${entry.email}</td>`;
-        const passwordCell = `<td class='border px-4 py-2'>${entry.password}</td>`;
-        const dobCell = `<td class='border px-4 py-2'>${entry.dob}</td>`;
-        const acceptedTermsCell = `<td class='border px-4 py-2'>${entry.acceptedTermsAndConditions ? 'Yes' : 'No'}</td>`;
-
-        const row = `<tr>${nameCell}${emailCell}${passwordCell}${dobCell}${acceptedTermsCell}</tr>`;
-        return row;
-    }).join("\n");
-
-    const table = `<table class="table-auto w-full"> 
-    <div id="user-entries" class="text-center">
-    <h1 class="mb-4"><b>USER ENTRIES</b></h1>
-    </div>
-        <tr>
-            <th class="px-4 py-2">Name</th>
-            <th class="px-4 py-2">Email</th>
-            <th class="px-4 py-2">Password</th>
-            <th class="px-4 py-2">DOB</th>
-            <th class="px-4 py-2">Accepted Terms?</th>
-        </tr>${tableEntries}
-    </table>`;
-
-    let details = document.getElementById("user-entries");
-    details.innerHTML = table;
-}
-
-const saveUserForm = (event) => {
+// function that writes data to the table from localStorage
+function table() {
+    const tableData = JSON.parse(localStorage.getItem("details")) || [];
+    let table = document.getElementById('table');
+    let tableContent = ``;
+    tableData.forEach(element => {
+      tableContent += `<tr class="border-2 border-stone-200 bg-stone-400">
+          <td class="py-2 px-4 text-center">${element.name}</td>
+          <td class="py-2 px-4 text-center">${element.email}</td>
+          <td class="py-2 px-4 text-center">${element.password}</td>
+          <td class="py-2 px-4 text-center">${element.dob}</td>
+          <td class="py-2 px-4 text-center">true</td>
+      </tr>`;
+    });
+  
+    table.innerHTML = `
+      <tr class="border-2 border-stone-200 bg-stone-400">
+        <th >Name</th>
+        <th>Email</th>
+        <th>Password</th>
+        <th>Dob</th>
+        <th>Accepted terms?</th>
+        </tr>
+      ${tableContent}`;
+  }
+  
+  // Function that is called when form is submitted
+  function submit(event) {
     event.preventDefault();
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const password = document.getElementById("password").value;
-    const dobInput = document.getElementById("dob");
-    const acceptedTermsAndConditions = document.getElementById("terms").checked;
-
-    
-    const dob = dobInput.value;
-    const userDob = new Date(dob);
-    const today = new Date();
-    const minDob = new Date(today.getFullYear() - 55, today.getMonth(), today.getDate());
-    const maxDob = new Date(today.getFullYear() - 18, today.getMonth(), today.getDate());
-
-    if (userDob < minDob || userDob > maxDob) {
-        alert("Please enter a valid date of birth between ages 18 and 55.");
-        return;
-    }
-
-    const entry = {
-        name,
-        email,
-        password,
-        dob,
-        acceptedTermsAndConditions
-    }
-
-    userEntries.push(entry);
-
-    localStorage.setItem("user-entries", JSON.stringify(userEntries));
-    displayEntries();
-}
-
-userForm.addEventListener("submit", saveUserForm);
-displayEntries();
+  
+    const user = {
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value,
+      dob: document.getElementById("dob").value,
+      password: document.getElementById("password").value,
+      tnc: document.getElementById("acceptTerms").value,
+    };
+  
+    let arr = JSON.parse(localStorage.getItem("details")) || [];
+    arr.push(user);
+    const data = arr;
+    localStorage.setItem("details", JSON.stringify(data));
+  
+    table(); // Call the table function after submitting the form
+  }
+  
+  // Set min and max dates
+  var today = new Date();
+  var maxDate = new Date(today);
+  maxDate.setFullYear(maxDate.getFullYear() - 18);
+  var minDate = new Date(today);
+  minDate.setFullYear(minDate.getFullYear() - 56);
+  
+  document.getElementById("dob").setAttribute("max", maxDate.toISOString().slice(0, 10));
+  document.getElementById("dob").setAttribute("min", minDate.toISOString().slice(0, 10));
+  
+  document.getElementById("form").addEventListener("submit", submit);
+  
+  // Table function call that adds all data in localStorage
+  table();
+  
